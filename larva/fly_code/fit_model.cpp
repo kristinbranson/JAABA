bf1f2c3582fc0249f618e49d72e2f6a71b922c17
Kristin Branson <bransonk@janelia.hhmi.org>
2011-08-23 11:09:50 -0400

    forgot to add ComputeMaxDependencyRadius.m. starting to add comments.

--- larva/fly_code/fit_model.cpp
+++ larva/fly_code/fit_model.cpp
@@ -6,12 +6,12 @@
 #include <highgui.h>
 #include <ml.h>  
 
-#ifdef WIN32
-#pragma comment(lib, "cv210")
-#pragma comment(lib, "cxcore210")
-#pragma comment(lib, "highgui210")
-#pragma comment(lib, "ml210")
-#endif
+//#ifdef WIN32
+//#pragma comment(lib, "cv210")
+//#pragma comment(lib, "cxcore210")
+//#pragma comment(lib, "highgui210")
+//#pragma comment(lib, "ml210")
+//#endif
 
 #define SCALE 20
 
