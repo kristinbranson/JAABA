a454d151aca0d611cdb2d26eaaa68052714d2dbf
Kristin Branson <bransonk@janelia.hhmi.org>
2011-08-23 17:31:30 -0400

    Added comments to ComputeWindowFeatures.m.

--- perframe/ComputeWindowFeatures.m
+++ perframe/ComputeWindowFeatures.m
@@ -1,37 +1,128 @@
 % [y,feature_names] = ComputeWindowFeatures(x,...)
 % 
 % Wrapper function that computes all the window features y for the input
-% one-dimensional time series x. 
+% one-dimensional time series x. Window features that can be computed by
+% this function include:
+% * mean: (transformation of) the average of the per-frame data within the
+% window. 
+% * min: minimum of the (transformation of) the per-frame data within the
+% window. 
+% * max: maximum of the (transformation of) the per-frame data within the
+% window. 
+% * hist: fraction of the (transformation of) the per-frame data within the
+% window within each bin specified by the input edges. 
+% * prctile: (transformation of) the percentiles of the per-frame data
+% within the window. 
+% * change: (transformation of) the difference between the mean of the
+% sub-window of per-frame data centered at the end of the window and the
+% mean of the sub-window of per-frame data centered at the start of the
+% window. 
+% * std: standard deviation of the per-frame data within the window. 
+% * harmonic: (transformation of) the correlation between the per-frame
+% data in the window and cosine functions for n / 2 periods, for n =
+% 1,...,num_harmonic. 
+% * diff_neighbor_mean: (transformation of) the difference between the
+% per-frame data at the current frame and the mean of the per-frame data in
+% the window around it. 
+% diff_neighbor_min: (transformation of) the difference between the
+% per-frame data at the current frame and the minimum of the per-frame data
+% in the window around it. 
+% diff_neighbor_max: (transformation of) the difference between the
+% per-frame data at the current frame and the maximum of the per-frame data
+% in the window around it. 
+% zscore_neighbors: (transformation of) the per-frame data at the current
+% frame z-scored by the statistics of the per-frame data in the in the
+% window around it.
+% 
+% Input: 
+%
+% x: 1 x NFRAMES array of per-frame data. 
+% 
+% Output: 
+%
+% y: NWINDOWFEATURES x NFRAMES matrix of window data, where y(i,t)
+% corresponds to window feature i and frame t. 
+% feature_names: 1 x NWINDOWFEATURES cell in which feature_names{i}
+% describes the ith window feature computed. feature_names{i} is itself a
+% cell that can be interpreted as pairs of a string description followed by
+% a value, e.g. 
+% {'stat','zscore_neighbors','trans','abs','radius',1,'offset',1}
 % 
 % Optional inputs:
 % 
-% windows: window offsets and radii to try ([radius1,offset1];...;[radiusn,offsetn])
+% DEFAULT WINDOW LOCATIONS:
+% These window locations are used if window locations are not specified on
+% a per-feature type basis. Default default values set by
+% SetDefaultWindowParameters.
+% Inputs interpreted by SetWindowParameters. 
+% 'windows': window offsets and radii to try ([radius1,offset1];...;[radiusn,offsetn])
 % if empty, then window_radii and window_offsets are used to specify
-% windows. default value: []. 
-% window_radii: if windows is empty, then the cross-product of window_radii
-% and window_offsets is used. if empty, then min_window_radius,
-% max_window_radius, and mwindow_radii are used to set
-%  windows,default_windows,...
-%   'window_radii',default_window_radii,'window_offsets',default_window_offsets,...
-%   'min_window_radius',default_min_window_radius,'max_window_radius',default_max_window_radius,'nwindow_radii',default_nwindow_radii,...
-%   'feature_types',feature_types,...
-%   'trans_types',default_trans_types,...
-%   'mean_params',mean_params,...
-%   'min_params',min_params,...
-%   'max_params',max_params,...
-%   'hist_params',hist_params,...
-%   'prctile_params',prctile_params,...
-%   'change_params',change_params,...
-%   'std_params',std_params,...
-%   'harmonic_params',harmonic_params,...
-%   'diff_neighbor_mean_params',diff_neighbor_mean_params,...
-%   'diff_neighbor_min_params',diff_neighbor_min_params,...
-%   'diff_neighbor_max_params',diff_neighbor_max_params,...
-%   'zscore_neighbors_params',zscore_neighbors_params,...
-%   'sanitycheck',SANITY_CHECK,...
-%   'docache',DOCACHE,...
-%   't0',[],'t1',[]);
-% Output y is nfeatures x nframes.
+% windows. if the window is [r,off], then the window feature at time t
+% will be computed from the window at t-r+off to t+r+off. default value: [].
+% 'window_radii': if windows is empty, then the cross-product of window_radii
+% and window_offsets is used to set windows. if empty, then
+% min_window_radius, max_window_radius, and nwindow_radii are used to set
+% window_radii. default value: [].
+% 'window_offsets': if windows is empty, then the cross-product of  window_radii
+% and window_offsets is used to set windows. window_offsets are relative to
+% radius, so the window corresponding to radius r_i and offset off_j_rel
+% will be [r_i,off_i=r_i*off_j_rel]. default value: [-1,0,1].
+% 'min_window_radius': if windows and window_radii are both empty, then
+% window_radii is set to nwindow_radii evenly spaced radii between
+% min_window_radii and max_window_radii. default value: 0
+% 'max_window_radius': see 'min_window_radius'. default value: 20.
+% 'nwindow_radii: see 'min_window_radius'. default value: 5. 
+%
+% 'feature_types': list of the types of window features to compute, chosen
+% from the list of window feature types listed above. 'feature_types' can also be 
+% the string 'all', in which case all features are computed. 
+%
+% 'trans_types': default types of transformations to apply for each feature
+% type, if not otherwise specified. 
+%
+% PER-FEATURE TYPE PARAMETERS:
+% parameters specific to each feature-type, passed to the
+% Compute*WindowFeatures functions. Default for all of these is {}. 
+% 'mean_params': cell array of parameters to be passed to
+% ComputeMeanWindowFeatures. 
+% 'min_params': cell array of parameters to be passed to
+% ComputeMinWindowFeatures. 
+% 'max_params': cell array of parameters to be passed to
+% ComputeMaxWindowFeatures. 
+% 'hist_params': cell array of parameters to be passed to
+% ComputeHistWindowFeatures. 
+% 'prctile_params': cell array of parameters to be passed to
+% ComputePrctileWindowFeatures. 
+% 'change_params': cell array of parameters to be passed to
+% ComputeChangeWindowFeatures. 
+% 'std_params': cell array of parameters to be passed to
+% ComputeStdWindowFeatures. 
+% 'harmonic_params': cell array of parameters to be passed to
+% ComputeHarmonicWindowFeatures. 
+% 'diff_neighbor_mean_params': cell array of parameters to be passed to
+% ComputeDiffNeighborMeanWindowFeatures. 
+% 'diff_neighbor_min_params': cell array of parameters to be passed to
+% ComputeDiffNeighborMinWindowFeatures. 
+% 'diff_neighbor_max_params': cell array of parameters to be passed to
+% ComputeDiffNeighborMaxWindowFeatures. 
+% 'zscore_neighbors_params': cell array of parameters to be passed to
+% ComputeZScoreNeighborsWindowFeatures. 
+% 
+% 'sanitycheck': whether to compute all the features a second time in the
+% obvious way to make sure that the optimized computations are correct.
+% default value: false. 
+%
+% 'docache': whether to cache computations that might be useful, e.g. the
+% mean computations are useful when computing the diff_neighbor_mean.
+% default value: true.
+%
+% 't0': first frame of per-frame data to compute window features for. if
+% empty, then window features are computed for all frames. default value:
+% []. 
+% 't1': last frame of per-frame data to compute window features for. if
+% empty, then window features are computed for all frames. default value:
+% []. 
+%
 
 function [y,feature_names] = ComputeWindowFeatures(x,varargin)
 
@@ -154,7 +245,7 @@ common_params = {'sanitycheck',SANITY_CHECK,...
 %% we'll need to compute means, stds mins, maxs for multiple features
 cache = InitializeCache();
 
-%% if t0 or t1 are input, then only compute in a boundary around t0, t1
+%% if t0 or t1 are input, then only compute in a boundary around t0 through t1
 % compute the maximum dependency radius and only compute up to there. 
 % there's more efficient ways to do this, but this is the simplest
 % modification to the code
