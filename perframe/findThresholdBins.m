311834c1bdabf28abd979ccae3e465d30704a73c
Mayank Kabra <kabram@kabram-ws.janelia.priv>
2011-11-09 20:45:14 -0500

    Fixed a few bugs and added the missing findThresholdBins.m

---
+++ perframe/findThresholdBins.m
@@ -0,0 +1,10 @@
+function bins = findThresholdBins(data,binVals)
+
+numDim = size(data,2);
+bins = zeros(size(data,2),size(data,1));
+parfor dim = 1:numDim
+  curD = data(:,dim);
+  curVals = binVals(:,dim);
+  curBins = sum(bsxfun(@gt,curD',curVals))+1;
+  bins(dim,:) = curBins;
+end
