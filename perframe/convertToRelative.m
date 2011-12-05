8af69b187cc2e579c17212b17a04521fd457250f
Mayank Kabra <kabram@janelia.hhmi.org>
2011-12-05 13:13:38 -0500

    Added relative features and refactored window feature computations.

---
+++ perframe/convertToRelative.m
@@ -0,0 +1,11 @@
+function modX = convertToRelative(x,relativeParams)
+
+bins = relativeParams.relativeBins;
+[~,modX] = histc(x,bins);
+modX(modX>numel(bins))=numel(bins);
+
+validX = ~isnan(modX) & (modX<length(bins));
+extra = x(validX)-bins(modX(validX));
+relExtra = extra./(bins(modX(validX)+1)-bins(modX(validX)));
+modX(validX) = modX(validX) + relExtra;
+modX = modX-1; % To make it go from 0 instead of 1
