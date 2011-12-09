79be5c909a7fa0939f7a7eb6802fb96a9825a044
Kristin Branson <bransonk@janelia.hhmi.org>
2011-12-09 15:29:01 -0500

    Fixed bug with computing harmonic window features with short trajectories.

--- perframe/HarmonicWindowCore.m
+++ perframe/HarmonicWindowCore.m
@@ -4,13 +4,19 @@ r = (w-1)/2;
 fil = cos(linspace(0,pi*num_harmonic_curr,w))/w*(num_harmonic_curr+1);
 % res is nan from 1:r, N-r+1:N
 res = myconv(x,fil,nan,'corr','same');
+nx = numel(x);
 % boundary conditions: smallw used for frame
 for smallr = 1:r-1,
   smallw = 2*smallr+1;
-  fil = cos(linspace(0,pi*num_harmonic_curr,smallw))/smallw*(num_harmonic_curr+1);
-  % start
-  res(smallr+1) = sum(fil.*x(1:smallw));
-  % end
-  res(end-smallr) = sum(fil.*x(end-smallw+1:end));
+  if smallw > nx,
+    res(smallr+1) = nan;
+    res(end-smallr) = nan;
+  else
+    fil = cos(linspace(0,pi*num_harmonic_curr,smallw))/smallw*(num_harmonic_curr+1);
+    % start
+    res(smallr+1) = sum(fil.*x(1:smallw));
+    % end
+    res(end-smallr) = sum(fil.*x(end-smallw+1:end));
+  end
 end
 
