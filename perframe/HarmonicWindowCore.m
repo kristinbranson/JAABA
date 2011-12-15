32d7c3c22f6aed434a1a931d67e21ab573c1fafb
Mayank Kabra <kabram@janelia.hhmi.org>
2011-12-15 10:08:09 -0500

    Modified HarmonicWindowCore to work on smaller inputs

--- perframe/HarmonicWindowCore.m
+++ perframe/HarmonicWindowCore.m
@@ -6,7 +6,7 @@ fil = cos(linspace(0,pi*num_harmonic_curr,w))/w*(num_harmonic_curr+1);
 res = myconv(x,fil,nan,'corr','same');
 % boundary conditions: smallw used for frame
 for smallr = 1:r-1,
-  smallw = 2*smallr+1;
+  smallw = min(2*smallr+1,numel(x));
   fil = cos(linspace(0,pi*num_harmonic_curr,smallw))/smallw*(num_harmonic_curr+1);
   % start
   res(smallr+1) = sum(fil.*x(1:smallw));
