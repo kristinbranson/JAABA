40fd5c9dd9c76fb416767f2257ead1dd8180d68b
Mayank Kabra <kabram@janelia.hhmi.org>
2011-12-15 18:19:33 -0500

    Removed try and catch blocks. Added color for cross validation. Fixed bugs in JLabelBatch.m

--- perframe/JLabelBatch.m
+++ perframe/JLabelBatch.m
@@ -6,5 +6,11 @@ if ~success,
   warning(msg);
 end
 
-data.AddExpDir(expName);
-data.PredictWholeMovie(data.nexps); 
+existingNdx = find(strcmp(expName,data.expdirs));
+if ~isempty(existingNdx)
+  data.PredictWholeMovie(existingNdx);
+else
+  data.AddExpDir(expName);
+  ndx = find(strcmp(expName,data.expdirs));
+  data.PredictWholeMovie(ndx);
+end
