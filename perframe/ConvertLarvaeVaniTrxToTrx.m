c0014aa36c50e8d8f8a9d8abe419911f4db7ce72
Kristin Branson <bransonk@janelia.hhmi.org>
2011-12-22 20:04:56 -0500

    functioned up the convertlarvaevani code.

--- perframe/ConvertLarvaeVaniTrxToTrx.m
+++ perframe/ConvertLarvaeVaniTrxToTrx.m
@@ -1,56 +1,29 @@
-%% set up path
+function ConvertLarvaeVaniTrxToTrx(expdir,varargin)
 
-[~,computername] = system('hostname');
-computername = strtrim(computername);
-
-switch computername,
-  
-  case 'bransonk-desktop',
-
-    JCtrax_path = '/groups/branson/home/bransonk/tracking/code/JCtrax';
-    configfilename = 'params/JLabelParams_bransonk-desktop.xml';
-    rootdatadir = '/groups/branson/home/bransonk/behavioranalysis/data/larvae_vani';
-    
-  case 'bransonk-lw2',
-
-    JCtrax_path = 'C:\Code\JCtrax';
-    configfilename = 'params\JLabelParamsKristinChase.xml';
-    rootdatadir = 'C:\Data\larvae_vani';
-
-  otherwise
-    
-    warning('Unknown computer %s. Paths may not be setup correctly',computername);
-    JCtrax_path = '/groups/branson/home/bransonk/tracking/code/JCtrax';
-    configfilename = 'params/JLabelParams_bransonk-desktop.xml';
-    rootdatadir = '/groups/branson/home/bransonk/behavioranalysis/data/larvae_vani';
-
-end
-
-addpath(genpath(fullfile(JCtrax_path,'pdollar_toolbox')));
-addpath(fullfile(JCtrax_path,'misc'));
-addpath(fullfile(JCtrax_path,'filehandling'));
-jlabelpath = fileparts(which('JLabel'));
-addpath(fullfile(jlabelpath,'compute_perframe_features'));
-
-
-%% parameters
-
-experiment_name = '20111213-144536';
+% parse optional parameters
 trxfilestr = 'JLabel_trx.mat';
 outmoviefilestr = 'JLabel_movie.avi';
 perframedirstr = 'perframe';
 
+[trxfilestr,outmoviefilestr,perframedirstr] = ...
+  myparse(varargin,...
+  'trxfilestr',trxfilestr,...
+  'outmoviefilestr',outmoviefilestr,...
+  'perframedirstr',perframedirstr);
+
+% get experiment_name from full path
+[~,experiment_name] = myfileparts(expdir);
 
 %% input file names
 
-inmoviename = fullfile(rootdatadir,experiment_name,sprintf('%s_video.avi',experiment_name));
-indataname = fullfile(rootdatadir,experiment_name,sprintf('%s_data.txt',experiment_name));
+inmoviename = fullfile(expdir,sprintf('%s_video.avi',experiment_name));
+indataname = fullfile(expdir,sprintf('%s_data.txt',experiment_name));
 
 %% output file names
 
-outtrxname = fullfile(rootdatadir,experiment_name,trxfilestr);
-outmoviename = fullfile(rootdatadir,experiment_name,outmoviefilestr);
-perframedir = fullfile(rootdatadir,experiment_name,perframedirstr);
+outtrxname = fullfile(expdir,trxfilestr);
+outmoviename = fullfile(expdir,outmoviefilestr);
+perframedir = fullfile(expdir,perframedirstr);
 
 %% convert trx
 
@@ -66,7 +39,7 @@ while true,
     break;
   end
   ss = strsplit(s,',');
-  ss = str2num(char(ss(1:17)));
+  ss = str2num(char(ss(1:17))); %#ok<ST2NM>
   indata.data(:,end+1) = ss;
 end
 %indata.data = [(0:size(indata.data,1)-1)',indata.data];
@@ -113,17 +86,8 @@ end
 % change in head-to-body angle
 fn = 'dhead2body';
 dangle = modrange(diff(indata.data(12,:)),-180,180);
-data = {dangle ./ trx.dt };
-units = struct('num','deg','den','s');
+data = {dangle ./ trx.dt }; %#ok<NASGU>
+units = struct('num','deg','den','s'); %#ok<NASGU>
 save(fullfile(perframedir,[fn,'.mat']),'data','units');
 
 % ADD MORE HERE
-
-%% DEBUG: plot the annotated movie
-
-for i = 1:1000,
-set(him,'CData',flipdim(readframe(i),1));
-set(hskel,'XData',trx.skeletonx(:,i),'YData',trx.skeletony(:,i));
-set(hcentroid,'XData',trx.x(i),'ydata',trx.y(i));
-pause(.1);
-end
