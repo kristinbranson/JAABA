c09de9d5e6a26362d4a759a2eeea2ac65754d1f5
Kristin Branson <bransonk@janelia.hhmi.org>
2012-01-23 18:58:18 -0500

    Updated code for converting Roian's mouse data to our experiment directory structure.

--- perframe/DebugConvertMouseTrx2Trx.m
+++ perframe/DebugConvertMouseTrx2Trx.m
@@ -3,11 +3,20 @@
 %% data location
 
 indatadir = '/groups/egnor/egnorlab/for kristin';
-expname = 'b6_popcage_16_110405_09.58.30.268';
+%inexpname = 'b6_popcage_16_110405_09.58.30.268';
+%inexpname = 'b6_popcage_16_110405_09.58.30.268.108000_216000';
+inexpname = 'b6_popcage_16_110405_09.58.30.268.216000_324000';
+%inexpname_seq = inexpname;
+%inexpname_seq = 'b6_popcage_16_110405_09.58.30.268 - frames 108000-216000';
+inexpname_seq = 'b6_popcage_16_110405_09.58.30.268 - frames 216000-324000';
+outexpname = regexprep(inexpname,'[^\w.]+','_');
+if ~strcmp(inexpname,outexpname),
+  fprintf('Converting %s to %s\n',inexpname,outexpname);
+end
 
-inmoviefilestr = [expname,'.seq'];
-inmoviefilestr2 = [expname,'.mat'];
-intrxfilestr = [expname,'t.mat'];
+inmoviefilestr = [inexpname_seq,'.seq'];
+inmoviefilestr2 = [inexpname,'.mat'];
+intrxfilestr = [inexpname,'t.mat'];
 
 outrootdatadir = '/groups/branson/home/bransonk/behavioranalysis/data/roian';
 outtrxfilestr = 'trx.mat';
@@ -16,7 +25,7 @@ outmoviefilestr2 = 'movie.mat';
 
 %% create the experiment directory
 
-outexpdir = fullfile(outrootdatadir,expname);
+outexpdir = fullfile(outrootdatadir,outexpname);
 if ~exist(outexpdir,'dir'),
   mkdir(outexpdir);
 end
@@ -24,14 +33,20 @@ end
 % soft link to .seq file
 inmoviename = fullfile(indatadir,inmoviefilestr);
 outmoviename = fullfile(outexpdir,outmoviefilestr);
+if ~exist(inmoviename,'file'),
+  error('File %s does not exist',inmoviename);
+end
 if ~exist(outmoviename,'file'),
-  unix(sprintf('ln -s "%s" %s',inmoviename,outmoviename));
+  unix(sprintf('ln -s "%s" "%s"',inmoviename,outmoviename));
 end
 
 inmoviename2 = fullfile(indatadir,inmoviefilestr2);
 outmoviename2 = fullfile(outexpdir,outmoviefilestr2);
+if ~exist(inmoviename2,'file'),
+  error('File %s does not exist',inmoviename2);
+end
 if ~exist(outmoviename2,'file'),
-  unix(sprintf('ln -s "%s" %s',inmoviename2,outmoviename2));
+  unix(sprintf('ln -s "%s" "%s"',inmoviename2,outmoviename2));
 end
 
 
