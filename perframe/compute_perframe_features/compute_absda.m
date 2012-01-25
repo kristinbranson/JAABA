d63c7823a7799f73348f52206a582ac1af59c836
Kristin Branson <bransonk@janelia.hhmi.org>
2012-01-25 16:38:28 -0500

    added code for computing change in major, minor axis lengths.

---
+++ perframe/compute_perframe_features/compute_absda.m
@@ -0,0 +1,11 @@
+% absolute change in quarter-major axis length
+function [data,units] = compute_absda(trx,n)
+
+flies = trx.exp2flies{n};
+nflies = numel(flies);
+data = cell(1,nflies);
+for i = 1:nflies,
+  fly = flies(i);
+  data{i} = abs(trx(fly).da);
+end
+units = parseunits('mm/s');
