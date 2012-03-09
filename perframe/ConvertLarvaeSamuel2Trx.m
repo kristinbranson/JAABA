9f847f87a74c0bdda2de7d9c4e8059b3807241d6
Kristin Branson <bransonk@janelia.hhmi.org>
2012-03-09 15:07:31 -0500

    added updatewingedfly.

--- perframe/ConvertLarvaeSamuel2Trx.m
+++ perframe/ConvertLarvaeSamuel2Trx.m
@@ -8,15 +8,16 @@ moviefilestr = 'movie.mmf';
 perframedirstr = 'perframe';
 DEBUG = false;
 makesoftlink = true;
+dotransposeimage = false;
 
-
-[trxfilestr,moviefilestr,perframedirstr,DEBUG,makesoftlink] = ...
+[trxfilestr,moviefilestr,perframedirstr,DEBUG,makesoftlink,dotransposeimage] = ...
   myparse(varargin,...
   'trxfilestr',trxfilestr,...
   'moviefilestr',moviefilestr,...
   'perframedirstr',perframedirstr,...
   'debug',DEBUG,...
-  'makesoftlink',makesoftlink); %#ok<ASGLU>
+  'makesoftlink',makesoftlink,...
+  'dotranspose',dotransposeimage); %#ok<ASGLU>
 
 %% parse experiment directory name
 [rootdatadir,inexperiment_name] = fileparts(inexpdir); %#ok<ASGLU>
@@ -70,6 +71,14 @@ trx = [];
 perframedata = struct;
 SCALE = 10; % centimeters instead of millimeters
 
+if dotransposeimage,
+  XIND = 1;
+  YIND = 2;
+else
+  XIND = 2;
+  YIND = 1;
+end
+
 for i = 1:nflies,
   
   fprintf('Larva %d...\n',i);
@@ -93,74 +102,109 @@ for i = 1:nflies,
   trk.pxpermm = 1/expdata.experiment_1.camcalinfo.realUnitsPerPixel;
   
   % convert to pixels, as everything else will be for plotting purposes
-  trk.x = expdata.experiment_1.camcalinfo.r2cX(trk.x_mm,trk.y_mm);
-  trk.y = expdata.experiment_1.camcalinfo.r2cY(trk.x_mm,trk.y_mm);
+  loc_px = cat(1,expdata.experiment_1.camcalinfo.r2cX(loc(1,:),loc(2,:)),...
+    expdata.experiment_1.camcalinfo.r2cY(loc(1,:),loc(2,:)));
+  trk.x = loc_px(XIND,:);
+  trk.y = loc_px(YIND,:);
 
   % midpoint
-  xmid_mm = mid(1,:);
-  ymid_mm = mid(2,:);
-  trk.xmid = expdata.experiment_1.camcalinfo.r2cX(xmid_mm,ymid_mm);
-  trk.ymid = expdata.experiment_1.camcalinfo.r2cY(xmid_mm,ymid_mm);
+  xmid_cm = mid(1,:);
+  ymid_cm = mid(2,:);
+  mid_px = cat(1,expdata.experiment_1.camcalinfo.r2cX(mid(1,:),mid(2,:)),...
+    expdata.experiment_1.camcalinfo.r2cY(mid(1,:),mid(2,:)));
+  trk.xmid = mid_px(XIND,:);
+  trk.ymid = mid_px(YIND,:);
 
   % head
-  xhead_mm = head(1,:);
-  yhead_mm = head(2,:);
-  trk.xhead = expdata.experiment_1.camcalinfo.r2cX(xhead_mm,yhead_mm);
-  trk.yhead = expdata.experiment_1.camcalinfo.r2cY(xhead_mm,yhead_mm);
+  xhead_cm = head(1,:);
+  yhead_cm = head(2,:);
+  head_px = cat(1,expdata.experiment_1.camcalinfo.r2cX(head(1,:),head(2,:)),...
+    expdata.experiment_1.camcalinfo.r2cY(head(1,:),head(2,:)));
+  trk.xhead = head_px(XIND,:);
+  trk.yhead = head_px(YIND,:);
   
   % tail
-  xtail_mm = tail(1,:);
-  ytail_mm = tail(2,:);
-  trk.xtail = expdata.experiment_1.camcalinfo.r2cX(xtail_mm,ytail_mm);
-  trk.ytail = expdata.experiment_1.camcalinfo.r2cY(xtail_mm,ytail_mm);
+  xtail_cm = tail(1,:);
+  ytail_cm = tail(2,:);
+  tail_px = cat(1,expdata.experiment_1.camcalinfo.r2cX(tail(1,:),tail(2,:)),...
+    expdata.experiment_1.camcalinfo.r2cY(tail(1,:),tail(2,:)));
+  trk.xtail = tail_px(XIND,:);
+  trk.ytail = tail_px(YIND,:);
 
   % spine
   nspinepts = max(cellfun(@(x) size(x,2),spine));
   isspine = ~cellfun(@isempty,spine);
-  xspine_mm = nan(nspinepts,trk.nframes);
-  yspine_mm = nan(nspinepts,trk.nframes);
-  xspine_mm(:,isspine) = cell2mat(cellfun(@(x) double(x(1,:)'),spine(isspine),'UniformOutput',false));
-  yspine_mm(:,isspine) = cell2mat(cellfun(@(x) double(x(2,:)'),spine(isspine),'UniformOutput',false));
-  trk.xspine = expdata.experiment_1.camcalinfo.r2cX(xspine_mm,yspine_mm);
-  trk.yspine = expdata.experiment_1.camcalinfo.r2cY(xspine_mm,yspine_mm);
-
+  xspine_cm = nan(nspinepts,trk.nframes);
+  yspine_cm = nan(nspinepts,trk.nframes);
+  xspine_cm(:,isspine) = cell2mat(cellfun(@(x) double(x(1,:)'),spine(isspine),'UniformOutput',false));
+  yspine_cm(:,isspine) = cell2mat(cellfun(@(x) double(x(2,:)'),spine(isspine),'UniformOutput',false));
+  if dotransposeimage,
+    trk.xspine = expdata.experiment_1.camcalinfo.r2cX(xspine_cm,yspine_cm);
+    trk.yspine = expdata.experiment_1.camcalinfo.r2cY(xspine_cm,yspine_cm);
+  else
+    trk.yspine = expdata.experiment_1.camcalinfo.r2cX(xspine_cm,yspine_cm);
+    trk.xspine = expdata.experiment_1.camcalinfo.r2cY(xspine_cm,yspine_cm);
+  end
+  
   % contour
-  xcontour_mm = cellfun(@(x) double(x(1,:)),contour,'UniformOutput',false);
-  ycontour_mm = cellfun(@(x) double(x(2,:)),contour,'UniformOutput',false);
-  trk.xcontour = cellfun(@(x,y) expdata.experiment_1.camcalinfo.r2cX(x,y), xcontour_mm,ycontour_mm,'UniformOutput',false);
-  trk.ycontour = cellfun(@(x,y) expdata.experiment_1.camcalinfo.r2cY(x,y), xcontour_mm,ycontour_mm,'UniformOutput',false);
+  xcontour_cm = cellfun(@(x) double(x(1,:)),contour,'UniformOutput',false);
+  ycontour_cm = cellfun(@(x) double(x(2,:)),contour,'UniformOutput',false);
+  if dotransposeimage,
+    trk.xcontour = cellfun(@(x,y) expdata.experiment_1.camcalinfo.r2cX(x,y), xcontour_cm,ycontour_cm,'UniformOutput',false);
+    trk.ycontour = cellfun(@(x,y) expdata.experiment_1.camcalinfo.r2cY(x,y), xcontour_cm,ycontour_cm,'UniformOutput',false);
+  else
+    trk.ycontour = cellfun(@(x,y) expdata.experiment_1.camcalinfo.r2cX(x,y), xcontour_cm,ycontour_cm,'UniformOutput',false);
+    trk.xcontour = cellfun(@(x,y) expdata.experiment_1.camcalinfo.r2cY(x,y), xcontour_cm,ycontour_cm,'UniformOutput',false);
+  end
 
   % covariance matrix is already in pixels; use it to get major, minor,
   % orientation
   S = cellfun(@(x) [x(1),x(2);x(2),x(3)], {expdata.experiment_1.track(i).pt.cov},'UniformOutput',false);
   S = cat(3,S{:});
+  if ~dotransposeimage,
+    S = [S(2,2,:),S(2,1,:);S(1,2,:),S(1,1,:)];
+  end
   [a,b,theta] = cov2ell(S);
   thetahead = atan2(trk.yhead-trk.ytail,trk.xhead-trk.xtail);
   thetaflip = modrange(thetahead+modrange(theta-thetahead,-pi/2,pi/2),-pi,pi);
   trk.a = a/2;
   trk.b = b/2;
   trk.theta = thetaflip;
- 
+  
   % convert a, b, theta to mm
-  x1 = trk.x + a.*cos(thetaflip);
-  x2 = trk.x - a.*cos(thetaflip);
-  y1 = trk.y + a.*sin(thetaflip);
-  y2 = trk.y - a.*sin(thetaflip);
-  x1_mm = expdata.experiment_1.camcalinfo.c2rX(x1,y1);
-  y1_mm = expdata.experiment_1.camcalinfo.c2rY(x1,y1);
-  x2_mm = expdata.experiment_1.camcalinfo.c2rX(x1,y1);
-  y2_mm = expdata.experiment_1.camcalinfo.c2rY(x2,y2);
-  trk.theta_mm = atan2(y2_mm-y1_mm,x2_mm-x1_mm);
-  trk.a_mm = sqrt((y2_mm-y1_mm).^2+(x2_mm-x1_mm).^2)/4;
-  x1 = trk.x + b.*cos(thetaflip+pi/2);
-  x2 = trk.x - b.*cos(thetaflip+pi/2);
-  y1 = trk.y + b.*sin(thetaflip+pi/2);
-  y2 = trk.y - b.*sin(thetaflip+pi/2);
-  x1_mm = expdata.experiment_1.camcalinfo.c2rX(x1,y1);
-  y1_mm = expdata.experiment_1.camcalinfo.c2rY(x1,y1);
-  x2_mm = expdata.experiment_1.camcalinfo.c2rX(x1,y1);
-  y2_mm = expdata.experiment_1.camcalinfo.c2rY(x2,y2);
-  trk.b_mm = sqrt((y2_mm-y1_mm).^2+(x2_mm-x1_mm).^2)/4;
+  if dotransposeimage,
+    x1 = trk.x + a.*cos(thetaflip);
+    x2 = trk.x - a.*cos(thetaflip);
+    y1 = trk.y + a.*sin(thetaflip);
+    y2 = trk.y - a.*sin(thetaflip);
+  else
+    y1 = trk.x + a.*cos(thetaflip);
+    y2 = trk.x - a.*cos(thetaflip);
+    x1 = trk.y + a.*sin(thetaflip);
+    x2 = trk.y - a.*sin(thetaflip);
+  end
+  x1_cm = expdata.experiment_1.camcalinfo.c2rX(x1,y1);
+  y1_cm = expdata.experiment_1.camcalinfo.c2rY(x1,y1);
+  x2_cm = expdata.experiment_1.camcalinfo.c2rX(x2,y2);
+  y2_cm = expdata.experiment_1.camcalinfo.c2rY(x2,y2);
+  trk.theta_mm = atan2(y2_cm-y1_cm,x2_cm-x1_cm);
+  trk.a_mm = sqrt((y2_cm-y1_cm).^2+(x2_cm-x1_cm).^2)/4;
+  if dotransposeimage,
+    x1 = trk.x + b.*cos(thetaflip+pi/2);
+    x2 = trk.x - b.*cos(thetaflip+pi/2);
+    y1 = trk.y + b.*sin(thetaflip+pi/2);
+    y2 = trk.y - b.*sin(thetaflip+pi/2);
+  else
+    y1 = trk.x + b.*cos(thetaflip+pi/2);
+    y2 = trk.x - b.*cos(thetaflip+pi/2);
+    x1 = trk.y + b.*sin(thetaflip+pi/2);
+    x2 = trk.y - b.*sin(thetaflip+pi/2);
+  end
+  x1_cm = expdata.experiment_1.camcalinfo.c2rX(x1,y1);
+  y1_cm = expdata.experiment_1.camcalinfo.c2rY(x1,y1);
+  x2_cm = expdata.experiment_1.camcalinfo.c2rX(x2,y2);
+  y2_cm = expdata.experiment_1.camcalinfo.c2rY(x2,y2);
+  trk.b_mm = sqrt((y2_cm-y1_cm).^2+(x2_cm-x1_cm).^2)/4;
     
   % all this was actually in centimeters, so convert to millimeters
   trk.x_mm = trk.x_mm*SCALE;
@@ -181,16 +225,16 @@ for i = 1:nflies,
   % TODO: compute some useful per-frame data
   
   % speed of each body point
-  perframedata.headspeed{i} = sqrt(diff(yhead_mm).^2+diff(xhead_mm).^2)*SCALE./trk.dt;
-  perframedata.midspeed{i} = sqrt(diff(ymid_mm).^2+diff(xmid_mm).^2)*SCALE./trk.dt;
-  perframedata.tailspeed{i} = sqrt(diff(ytail_mm).^2+diff(xtail_mm).^2)*SCALE./trk.dt;
+  perframedata.headspeed{i} = sqrt(diff(yhead_cm).^2+diff(xhead_cm).^2)*SCALE./trk.dt;
+  perframedata.midspeed{i} = sqrt(diff(ymid_cm).^2+diff(xmid_cm).^2)*SCALE./trk.dt;
+  perframedata.tailspeed{i} = sqrt(diff(ytail_cm).^2+diff(xtail_cm).^2)*SCALE./trk.dt;
 
   % head direction
-  perframedata.headdir{i} = atan2(yhead_mm-ymid_mm,xhead_mm-xmid_mm);
+  perframedata.headdir{i} = atan2(yhead_cm-ymid_cm,xhead_cm-xmid_cm);
   perframedata.dheaddir{i} = diff(perframedata.headdir{i})./trk.dt;
   
   % tail to mid direction
-  perframedata.taildir{i} = atan2(ymid_mm-ytail_mm,xmid_mm-xtail_mm);
+  perframedata.taildir{i} = atan2(ymid_cm-ytail_cm,xmid_cm-xtail_cm);
   perframedata.dtaildir{i} = diff(perframedata.taildir{i})./trk.dt;
   
   % bend
@@ -200,66 +244,68 @@ for i = 1:nflies,
   perframedata.dbend{i} = modrange(diff(perframedata.bend{i}),-pi,pi);
   
   % skeleton length
-  perframedata.spinelength{i} = sum(sqrt(diff(yspine_mm).^2+diff(xspine_mm).^2))*SCALE;
+  perframedata.spinelength{i} = sum(sqrt(diff(yspine_cm).^2+diff(xspine_cm).^2))*SCALE;
 
   % change in skeleton length
   perframedata.dspinelength{i} = diff(perframedata.spinelength{i}) ./ trk.dt;
   
   % velocity relative to head direction
-  perframedata.headvelforward_headdir{i} = (diff(xhead_mm).*cos(perframedata.headdir{i}(1:end-1)) + ...
-    diff(yhead_mm).*sin(perframedata.headdir{i}(1:end-1))) * SCALE ./ trk.dt;
-  perframedata.headvelsideways_headdir{i} = (diff(xhead_mm).*cos(perframedata.headdir{i}(1:end-1)+pi/2) + ...
-    diff(yhead_mm).*sin(perframedata.headdir{i}(1:end-1))+pi/2) * SCALE ./ trk.dt;
-  perframedata.midvelforward_headdir{i} = (diff(xmid_mm).*cos(perframedata.headdir{i}(1:end-1)) + ...
-    diff(ymid_mm).*sin(perframedata.headdir{i}(1:end-1))) * SCALE ./ trk.dt;
-  perframedata.midvelsideways_headdir{i} = (diff(xmid_mm).*cos(perframedata.headdir{i}(1:end-1)+pi/2) + ...
-    diff(ymid_mm).*sin(perframedata.headdir{i}(1:end-1))+pi/2) * SCALE ./ trk.dt;
-  perframedata.tailvelforward_headdir{i} = (diff(xtail_mm).*cos(perframedata.headdir{i}(1:end-1)) + ...
-    diff(ytail_mm).*sin(perframedata.headdir{i}(1:end-1))) * SCALE ./ trk.dt;
-  perframedata.tailvelsideways_headdir{i} = (diff(xtail_mm).*cos(perframedata.headdir{i}(1:end-1)+pi/2) + ...
-    diff(ytail_mm).*sin(perframedata.headdir{i}(1:end-1))+pi/2) * SCALE ./ trk.dt;
+  perframedata.headvelforward_headdir{i} = (diff(xhead_cm).*cos(perframedata.headdir{i}(1:end-1)) + ...
+    diff(yhead_cm).*sin(perframedata.headdir{i}(1:end-1))) * SCALE ./ trk.dt;
+  perframedata.headvelsideways_headdir{i} = (diff(xhead_cm).*cos(perframedata.headdir{i}(1:end-1)+pi/2) + ...
+    diff(yhead_cm).*sin(perframedata.headdir{i}(1:end-1))+pi/2) * SCALE ./ trk.dt;
+  perframedata.midvelforward_headdir{i} = (diff(xmid_cm).*cos(perframedata.headdir{i}(1:end-1)) + ...
+    diff(ymid_cm).*sin(perframedata.headdir{i}(1:end-1))) * SCALE ./ trk.dt;
+  perframedata.midvelsideways_headdir{i} = (diff(xmid_cm).*cos(perframedata.headdir{i}(1:end-1)+pi/2) + ...
+    diff(ymid_cm).*sin(perframedata.headdir{i}(1:end-1))+pi/2) * SCALE ./ trk.dt;
+  perframedata.tailvelforward_headdir{i} = (diff(xtail_cm).*cos(perframedata.headdir{i}(1:end-1)) + ...
+    diff(ytail_cm).*sin(perframedata.headdir{i}(1:end-1))) * SCALE ./ trk.dt;
+  perframedata.tailvelsideways_headdir{i} = (diff(xtail_cm).*cos(perframedata.headdir{i}(1:end-1)+pi/2) + ...
+    diff(ytail_cm).*sin(perframedata.headdir{i}(1:end-1))+pi/2) * SCALE ./ trk.dt;
   perframedata.centroidvelforward_headdir{i} = (diff(trk.x_mm).*cos(perframedata.headdir{i}(1:end-1)) + ...
     diff(trk.y_mm).*sin(perframedata.headdir{i}(1:end-1))) ./ trk.dt;
   perframedata.centroidvelsideways_headdir{i} = (diff(trk.x_mm).*cos(perframedata.headdir{i}(1:end-1)+pi/2) + ...
     diff(trk.y_mm).*sin(perframedata.headdir{i}(1:end-1))+pi/2) ./ trk.dt;
 
-  % velocity relative to tail dir
-  perframedata.headvelforward_taildir{i} = (diff(xhead_mm).*cos(perframedata.taildir{i}(1:end-1)) + ...
-    diff(yhead_mm).*sin(perframedata.taildir{i}(1:end-1))) * SCALE ./ trk.dt;
-  perframedata.headvelsideways_taildir{i} = (diff(xhead_mm).*cos(perframedata.taildir{i}(1:end-1)+pi/2) + ...
-    diff(yhead_mm).*sin(perframedata.taildir{i}(1:end-1))+pi/2) * SCALE ./ trk.dt;
-  perframedata.midvelforward_taildir{i} = (diff(xmid_mm).*cos(perframedata.taildir{i}(1:end-1)) + ...
-    diff(ymid_mm).*sin(perframedata.taildir{i}(1:end-1))) * SCALE ./ trk.dt;
-  perframedata.midvelsideways_taildir{i} = (diff(xmid_mm).*cos(perframedata.taildir{i}(1:end-1)+pi/2) + ...
-    diff(ymid_mm).*sin(perframedata.taildir{i}(1:end-1))+pi/2) * SCALE ./ trk.dt;
-  perframedata.tailvelforward_taildir{i} = (diff(xtail_mm).*cos(perframedata.taildir{i}(1:end-1)) + ...
-    diff(ytail_mm).*sin(perframedata.taildir{i}(1:end-1))) * SCALE ./ trk.dt;
-  perframedata.tailvelsideways_taildir{i} = (diff(xtail_mm).*cos(perframedata.taildir{i}(1:end-1)+pi/2) + ...
-    diff(ytail_mm).*sin(perframedata.taildir{i}(1:end-1))+pi/2) * SCALE ./ trk.dt;
-  perframedata.centroidvelforward_taildir{i} = (diff(trk.x_mm).*cos(perframedata.taildir{i}(1:end-1)) + ...
-    diff(trk.y_mm).*sin(perframedata.taildir{i}(1:end-1))) ./ trk.dt;
-  perframedata.centroidvelsideways_taildir{i} = (diff(trk.x_mm).*cos(perframedata.taildir{i}(1:end-1)+pi/2) + ...
-    diff(trk.y_mm).*sin(perframedata.taildir{i}(1:end-1))+pi/2) ./ trk.dt;
-
-  % velocity relative to ellipse orientation
-  perframedata.headvelforward_bodydir{i} = (diff(xhead_mm).*cos(trk.theta_mm(1:end-1)) + ...
-    diff(yhead_mm).*sin(trk.theta_mm(1:end-1))) * SCALE ./ trk.dt;
-  perframedata.headvelsideways_bodydir{i} = (diff(xhead_mm).*cos(trk.theta_mm(1:end-1)+pi/2) + ...
-    diff(yhead_mm).*sin(trk.theta_mm(1:end-1))+pi/2) * SCALE ./ trk.dt;
-  perframedata.midvelforward_bodydir{i} = (diff(xmid_mm).*cos(trk.theta_mm(1:end-1)) + ...
-    diff(ymid_mm).*sin(trk.theta_mm(1:end-1))) * SCALE ./ trk.dt;
-  perframedata.midvelsideways_bodydir{i} = (diff(xmid_mm).*cos(trk.theta_mm(1:end-1)+pi/2) + ...
-    diff(ymid_mm).*sin(trk.theta_mm(1:end-1))+pi/2) * SCALE ./ trk.dt;
-  perframedata.tailvelforward_bodydir{i} = (diff(xtail_mm).*cos(trk.theta_mm(1:end-1)) + ...
-    diff(ytail_mm).*sin(trk.theta_mm(1:end-1))) * SCALE ./ trk.dt;
-  perframedata.tailvelsideways_bodydir{i} = (diff(xtail_mm).*cos(trk.theta_mm(1:end-1)+pi/2) + ...
-    diff(ytail_mm).*sin(trk.theta_mm(1:end-1))+pi/2) * SCALE ./ trk.dt;
-  perframedata.centroidvelforward_bodydir{i} = (diff(trk.x_mm).*cos(trk.theta_mm(1:end-1)) + ...
-    diff(trk.y_mm).*sin(trk.theta_mm(1:end-1))) ./ trk.dt;
-  perframedata.centroidvelsideways_bodydir{i} = (diff(trk.x_mm).*cos(trk.theta_mm(1:end-1)+pi/2) + ...
-    diff(trk.y_mm).*sin(trk.theta_mm(1:end-1))+pi/2) ./ trk.dt;
+%   % velocity relative to tail dir
+%   perframedata.headvelforward_taildir{i} = (diff(xhead_mm).*cos(perframedata.taildir{i}(1:end-1)) + ...
+%     diff(yhead_mm).*sin(perframedata.taildir{i}(1:end-1))) * SCALE ./ trk.dt;
+%   perframedata.headvelsideways_taildir{i} = (diff(xhead_mm).*cos(perframedata.taildir{i}(1:end-1)+pi/2) + ...
+%     diff(yhead_mm).*sin(perframedata.taildir{i}(1:end-1))+pi/2) * SCALE ./ trk.dt;
+%   perframedata.midvelforward_taildir{i} = (diff(xmid_mm).*cos(perframedata.taildir{i}(1:end-1)) + ...
+%     diff(ymid_mm).*sin(perframedata.taildir{i}(1:end-1))) * SCALE ./ trk.dt;
+%   perframedata.midvelsideways_taildir{i} = (diff(xmid_mm).*cos(perframedata.taildir{i}(1:end-1)+pi/2) + ...
+%     diff(ymid_mm).*sin(perframedata.taildir{i}(1:end-1))+pi/2) * SCALE ./ trk.dt;
+%   perframedata.tailvelforward_taildir{i} = (diff(xtail_mm).*cos(perframedata.taildir{i}(1:end-1)) + ...
+%     diff(ytail_mm).*sin(perframedata.taildir{i}(1:end-1))) * SCALE ./ trk.dt;
+%   perframedata.tailvelsideways_taildir{i} = (diff(xtail_mm).*cos(perframedata.taildir{i}(1:end-1)+pi/2) + ...
+%     diff(ytail_mm).*sin(perframedata.taildir{i}(1:end-1))+pi/2) * SCALE ./ trk.dt;
+%   perframedata.centroidvelforward_taildir{i} = (diff(trk.x_mm).*cos(perframedata.taildir{i}(1:end-1)) + ...
+%     diff(trk.y_mm).*sin(perframedata.taildir{i}(1:end-1))) ./ trk.dt;
+%   perframedata.centroidvelsideways_taildir{i} = (diff(trk.x_mm).*cos(perframedata.taildir{i}(1:end-1)+pi/2) + ...
+%     diff(trk.y_mm).*sin(perframedata.taildir{i}(1:end-1))+pi/2) ./ trk.dt;
+% 
+%   % velocity relative to ellipse orientation
+%   perframedata.headvelforward_bodydir{i} = (diff(xhead_mm).*cos(trk.theta_mm(1:end-1)) + ...
+%     diff(yhead_mm).*sin(trk.theta_mm(1:end-1))) * SCALE ./ trk.dt;
+%   perframedata.headvelsideways_bodydir{i} = (diff(xhead_mm).*cos(trk.theta_mm(1:end-1)+pi/2) + ...
+%     diff(yhead_mm).*sin(trk.theta_mm(1:end-1))+pi/2) * SCALE ./ trk.dt;
+%   perframedata.midvelforward_bodydir{i} = (diff(xmid_mm).*cos(trk.theta_mm(1:end-1)) + ...
+%     diff(ymid_mm).*sin(trk.theta_mm(1:end-1))) * SCALE ./ trk.dt;
+%   perframedata.midvelsideways_bodydir{i} = (diff(xmid_mm).*cos(trk.theta_mm(1:end-1)+pi/2) + ...
+%     diff(ymid_mm).*sin(trk.theta_mm(1:end-1))+pi/2) * SCALE ./ trk.dt;
+%   perframedata.tailvelforward_bodydir{i} = (diff(xtail_mm).*cos(trk.theta_mm(1:end-1)) + ...
+%     diff(ytail_mm).*sin(trk.theta_mm(1:end-1))) * SCALE ./ trk.dt;
+%   perframedata.tailvelsideways_bodydir{i} = (diff(xtail_mm).*cos(trk.theta_mm(1:end-1)+pi/2) + ...
+%     diff(ytail_mm).*sin(trk.theta_mm(1:end-1))+pi/2) * SCALE ./ trk.dt;
+%   perframedata.centroidvelforward_bodydir{i} = (diff(trk.x_mm).*cos(trk.theta_mm(1:end-1)) + ...
+%     diff(trk.y_mm).*sin(trk.theta_mm(1:end-1))) ./ trk.dt;
+%   perframedata.centroidvelsideways_bodydir{i} = (diff(trk.x_mm).*cos(trk.theta_mm(1:end-1)+pi/2) + ...
+%     diff(trk.y_mm).*sin(trk.theta_mm(1:end-1))+pi/2) ./ trk.dt;
   
 end
+
+
 
 %% create the trx file
 
