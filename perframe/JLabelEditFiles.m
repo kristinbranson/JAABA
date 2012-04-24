b1be0c25c996acd80820b4721cad45677c75d74d
Mayank Kabra <kabram@janelia.hhmi.org>
2012-04-24 09:54:01 -0400

    Fixed a few minor bugs. And coded up most part of ProjectManager.

--- perframe/JLabelEditFiles.m
+++ perframe/JLabelEditFiles.m
@@ -889,43 +889,6 @@ if ~isempty(featureList),
 end
 xmlwrite(fname,docNode);
 
-function node = createXMLNode(docNode,name,value)
-
-node = docNode.createElement(name);
-
-fs = fieldnames(value);
-for ndx = 1:numel(fs)
-  curVal = value.(fs{ndx});
-  switch class(curVal)
-    case 'struct'
-      node.appendChild(createXMLNode(docNode,fs{ndx},value.(fs{ndx})));
-    case 'cell'
-      valStr = curVal{1};
-      for vNdx = 2:numel(curVal)
-        valStr = [valStr ',' curVal{vNdx}];
-      end
-      node.setAttribute(fs{ndx},valStr);
-    case 'double'
-      valStr = num2str(curVal(1));
-      for vNdx = 2:numel(curVal)
-        valStr = [valStr ',' num2str(curVal(vNdx))];
-      end      
-      node.setAttribute(fs{ndx},valStr);
-    case 'logical'
-      valStr = num2str(curVal(1));
-      for vNdx = 2:numel(curVal)
-        valStr = [valStr ',' num2str(curVal(vNdx))];
-      end      
-      node.setAttribute(fs{ndx},valStr);
-    case 'char'
-      valStr = curVal;
-      node.setAttribute(fs{ndx},valStr);
-    otherwise
-      fprintf('Unknown type');
-  end
-      
-end
-
 
 function SetLabelingMode(handles)
 contents = cellstr(get(handles.popupmode,'String'));
