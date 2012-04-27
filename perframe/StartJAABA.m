6cc1638a3b01adb53817568c63661762d5c60567
Mayank Kabra <kabram@janelia.hhmi.org>
2012-04-27 14:48:17 -0400

    Added Scores to Perframe capability to JLabelData

--- perframe/StartJAABA.m
+++ perframe/StartJAABA.m
@@ -7,5 +7,8 @@ addpath(fullfile(baseDir,'filehandling'));
 jlabelpath = fileparts(which('JLabel'));
 addpath(fullfile(jlabelpath,'compute_perframe_features'));
 
+if matlabpool('size')<1
+  matlabpool open;
+end
 % Start JAABA.
 JLabel();
