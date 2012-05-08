886300fceb546753e45c980989b4c54599fa42a0
Kristin Branson <bransonk@janelia.hhmi.org>
2012-05-07 21:43:39 -0400

    Fixed bug in groundtruthing -- current fly wasn't being considered in groundtruthing score.

--- perframe/JLabelData.m
+++ perframe/JLabelData.m
@@ -4965,6 +4965,7 @@ classdef JLabelData < handle
     
     function crossError = GetGTPerformance(obj)
       % Computes the performance on the GT data.
+      obj.StoreLabels();
       crossError.numbers = zeros(4,3);
       crossError.frac = zeros(4,3);
       
