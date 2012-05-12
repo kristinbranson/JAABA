cc43cb666f5826266c55235d871cf8681b1f625f
Kristin Branson <bransonk@janelia.hhmi.org>
2012-05-11 20:36:07 -0400

    Compiled version did not work with parallel computing on a different computer. Messed with stuff until it did. I had to manually put the .settings file in the distrib directory.

--- perframe/StartJAABA_compiled.m
+++ perframe/StartJAABA_compiled.m
@@ -1,9 +1,25 @@
 % JAABA start up script.
 
 % Initialize all the paths.
-if matlabpool('size')<1
-  matlabpool open;
+% try
+%   if matlabpool('size')<1
+%     matlabpool open;
+%   end
+% catch ME
+%   warndlg(sprintf('Could not start matlabpool with just "matlabpool open": %s',getReport(ME)));
+% end
+
+try
+  if isdeployed,
+    if ispc,
+      setmcruserdata('ParallelProfile','ParallelComputingConfiguration_Local_Win4.settings');
+    end
+    matlabpool('open');
+  end
+catch ME,
+  uiwait(warndlg('Error starting parallel computing: %s',getReport(ME)));
 end
+
 % Start JAABA.
 
 if ismac,
