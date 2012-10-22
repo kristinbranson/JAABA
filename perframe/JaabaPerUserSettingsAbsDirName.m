2fa5d870618157860d501596fb76b458b6414c40
Mayank Kabra <kabram@janelia.hhmi.org>
2012-10-22 19:14:38 -0400

    bug fix.

--- perframe/JaabaPerUserSettingsAbsDirName.m
+++ perframe/JaabaPerUserSettingsAbsDirName.m
@@ -10,27 +10,28 @@ else
   jaabadir = 'jaaba';
 end
 if isdeployed,
-  if exist(fullfile(settingsDirName,jaabatxt),'file')
-    ff = fopen(fullfile(settingsDirName,jaabatxt));
-    jaabaPerUserDirName = fgetl(ff);
-    fclose(ff);
-  else
-    jaabaPerUserDirName = uigetdir(settingsDirName,'Select a file to store JAABA configuration files');
-    if ~exist(jaabaPerUserDirName)
-      mkdir(jaabaPerUserDirName);
-    end
-    ff  = fopen(fullfile(settingsDirName,jaabatxt),'w');
-    fprintf(ff,'%s',jaabaPerUserDirName);
-    fclose(ff);
-    if jaabaPerUserDirName == 0,
-      error('Cannot continue without a settings dir');
-    end
-    dd = dir(fullfile(pwd,'params','*.xml'));
-    for ndx = 1:numel(dd)
-      copyfile(fullfile(pwd,'params',dd(ndx).name),....
-        fullfile(jaabaPerUserDirName,dd(ndx).name));
-    end
-  end
+%   if exist(fullfile(settingsDirName,jaabatxt),'file')
+%     ff = fopen(fullfile(settingsDirName,jaabatxt));
+%     jaabaPerUserDirName = fgetl(ff);
+%     fclose(ff);
+%   else
+%     jaabaPerUserDirName = uigetdir(settingsDirName,'Select a file to store JAABA configuration files');
+%     if ~exist(jaabaPerUserDirName)
+%       mkdir(jaabaPerUserDirName);
+%     end
+%     ff  = fopen(fullfile(settingsDirName,jaabatxt),'w');
+%     fprintf(ff,'%s',jaabaPerUserDirName);
+%     fclose(ff);
+%     if jaabaPerUserDirName == 0,
+%       error('Cannot continue without a settings dir');
+%     end
+%     dd = dir(fullfile(pwd,'params','*.xml'));
+%     for ndx = 1:numel(dd)
+%       copyfile(fullfile(pwd,'params',dd(ndx).name),....
+%         fullfile(jaabaPerUserDirName,dd(ndx).name));
+%     end
+%   end
+  jaabaPerUserDirName = 'params';
 else
   jaabaPerUserDirName=fullfile(settingsDirName,jaabadir);
 end
