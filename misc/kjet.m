71c1ec35c2b88e3761993d9b39cb2fda6856ee41
Kristin Branson <bransonk@janelia.hhmi.org>
2012-12-01 21:41:26 -0500

    finished removing code we're not using by other people. working on license and readme.

--- misc/kjet.m
+++ misc/kjet.m
@@ -1,16 +1,5 @@
 function J = kjet(m)
-%JET    Variant of HSV
-%   JET(M), a variant of HSV(M), is an M-by-3 matrix containing
-%   the default colormap used by CONTOUR, SURF and PCOLOR.
-%   The colors begin with dark blue, range through shades of
-%   blue, cyan, green, yellow and red, and end with dark red.
-%   JET, by itself, is the same length as the current figure's
-%   colormap. If no figure exists, MATLAB creates one.
-%
-%   See also HSV, HOT, PINK, FLAG, COLORMAP, RGBPLOT.
-
-%   Copyright 1984-2004 The MathWorks, Inc.
-%   $Revision: 5.7.4.2 $  $Date: 2005/06/21 19:31:40 $
+% almost the same as JET, except the first color is black
 
 if nargin < 1
    m = size(get(gcf,'colormap'),1);
--- misc/weighted_prctile.m
+++ misc/weighted_prctile.m
@@ -1,34 +1,6 @@
 function y = weighted_prctile(x,p,w,dim,issorted)
-% WEIGHTED_PRCTILE Percentiles of a sample.
-%   Y = WEIGHTED_PRCTILE(X,P,W) returns percentiles of the values in X.  P is a scalar
-%   or a vector of percent values.  When X is a vector, Y is the same size
-%   as P, and Y(i) contains the P(i)-th percentile.  When X is a matrix,
-%   the i-th row of Y contains the P(i)-th percentiles of each column of X.
-%   For N-D arrays, WEIGHTED_PRCTILE operates along the first non-singleton
-%   dimension.
-%
-%   Y = WEIGHTED_PRCTILE(X,P,W,DIM) calculates percentiles along dimension DIM.  The
-%   DIM'th dimension of Y has length LENGTH(P).
-%
-%   Percentiles are specified using percentages, from 0 to 100.  For an N
-%   element vector X, WEIGHTED_PRCTILE computes percentiles as follows:
-%      1) The sorted values in X are taken as the 100*(0.5/N), 100*(1.5/N),
-%         ..., 100*((N-0.5)/N) percentiles.
-%      2) Linear interpolation is used to compute percentiles for percent
-%         values between 100*(0.5/N) and 100*((N-0.5)/N)
-%      3) The minimum or maximum values in X are assigned to percentiles
-%         for percent values outside that range.
-%
-%   WEIGHTED_PRCTILE treats NaNs as missing values, and removes them.
-%
-%   Examples:
-%      y = weighted_prctile(x,50); % the median of x
-%      y = weighted_prctile(x,[2.5 25 50 75 97.5]); % a useful summary of x
-%
-%   See also IQR, MEDIAN, NANMEDIAN, QUANTILE.
+% similar to MATLAB's prctile, but takes weights
 
-%   Copyright 1993-2004 The MathWorks, Inc.
-%   $Revision: 2.12.4.5 $  $Date: 2004/06/25 18:52:56 $
 
 if ~isvector(p) || numel(p) == 0
     error('stats:prctile:BadPercents', ...
