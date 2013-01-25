b75a277e3d72b5ba33e097bfde787df7550c0934
Adam L. Taylor <adamlyletaylor@gmail.com>
2013-01-24 21:28:59 -0500

    Still in middle of getting labels to load from .jab files, but fixed most issues with button color on Mac.

--- perframe/PrepareJAABAData.m
+++ perframe/PrepareJAABAData.m
@@ -58,12 +58,14 @@ handles = InitializeGUI(handles);
 
 buttons = findall(hObject,'Style','pushbutton');
 for ndx = 1:numel(buttons)
-  SetButtonImage(buttons(ndx));
+  %SetButtonImage(buttons(ndx));
+  adjustNonLabelButtonColor(buttons(ndx));
 end
 
 buttons = findall(hObject,'Style','togglebutton');
 for ndx = 1:numel(buttons)
-  SetButtonImage(buttons(ndx));
+  %SetButtonImage(buttons(ndx));
+  adjustNonLabelButtonColor(buttons(ndx));
 end
 
 if ismac, % On mac change the foreground color to black.
