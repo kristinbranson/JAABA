93a632db39c0538e6cf0cc2d114e1649701dc9fd
Mayank Kabra <kabram@janelia.hhmi.org>
2013-01-29 09:49:06 -0500

    Fixed bugs in JAABAPlot and rewrote the empty script JAABAScores2CSV

---
+++ perframe/JAABAScores2CSV.m
@@ -0,0 +1,18 @@
+function JAABAScores2CSV(scorefile,outfile)
+
+% function JAABAScores2CSV(scorefile,outfile)
+% Convert JAABA Scores to comma separated values.
+
+
+if ~exist(scorefile,'file'),
+  error('Scorefile %s doesn''t exist\n',scorefile);
+end
+Q = load(scorefile);
+
+fid = fopen(outfile,'w');
+
+for ndx = 1:numel(Q.allScores.scores)
+  for t = Q.allScores.tStart(ndx):Q.allScores.tEnd(ndx)
+    fprintf(fid,'%d %d %.4f\n',ndx,t,Q.allScores.scores{ndx}(t));
+  end
+end
