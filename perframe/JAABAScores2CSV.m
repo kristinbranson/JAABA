f8570505fec768f0a1e9dd0a9a023bd2bfe468fd
Mayank Kabra <kabram@janelia.hhmi.org>
2013-01-29 09:51:05 -0500

    Minor change to Scores2CSV script

--- perframe/JAABAScores2CSV.m
+++ perframe/JAABAScores2CSV.m
@@ -2,6 +2,8 @@ function JAABAScores2CSV(scorefile,outfile)
 
 % function JAABAScores2CSV(scorefile,outfile)
 % Convert JAABA Scores to comma separated values.
+% The format of CSV file is Animal Num, Frame Num, Score Value
+% (unnormalized).
 
 
 if ~exist(scorefile,'file'),
@@ -13,6 +15,6 @@ fid = fopen(outfile,'w');
 
 for ndx = 1:numel(Q.allScores.scores)
   for t = Q.allScores.tStart(ndx):Q.allScores.tEnd(ndx)
-    fprintf(fid,'%d %d %.4f\n',ndx,t,Q.allScores.scores{ndx}(t));
+    fprintf(fid,'%d,%d,%.4f\n',ndx,t,Q.allScores.scores{ndx}(t));
   end
 end
