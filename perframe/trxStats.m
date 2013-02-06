7c96389845e0ff566b841d4c20d5db5066091df0
Mayank Kabra <kabram@janelia.hhmi.org>
2013-02-06 15:30:06 -0500

    Modified corner detection for vivek. It uses the square constraint

--- perframe/trxStats.m
+++ perframe/trxStats.m
@@ -3,7 +3,9 @@ function [nanimals, tracklens] = trxStats(trxfile)
 % Reads a trx file and outputs the number of trajectories in the file and
 % track length of each trajectory.
 
-SetUpJAABAPath();
+if ~exist('load_tracks','builtin'),
+  SetUpJAABAPath();
+end
 Q = load_tracks(trxfile);
 nanimals = numel(Q);
 tracklens = zeros(1,nanimals);
