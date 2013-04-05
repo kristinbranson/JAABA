c3fd1eed6146e8609a453f5a13c6534aa84c475e
Adam L. Taylor <adamlyletaylor@gmail.com>
2013-04-05 12:07:04 -0400

    Fixed converter to support sublexicons.  Seems to work.

--- misc/everythingFromOldStyleProjectAndClassifier.m
+++ misc/everythingFromOldStyleProjectAndClassifier.m
@@ -1,7 +1,7 @@
 function everythingParams= ...
   everythingFromOldStyleProjectAndClassifier(projectParams, ...
                                              classifierParams)
-                                           
+
 everythingParams=struct();
 
 % get the featureLexicon from the relevant file
@@ -19,7 +19,7 @@ featureLexicon = ReadXMLParams(featureLexiconFileNameAbs);
   getFeatureLexiconListsFromXML();
 isSameLexicon=strcmp(featureLexiconFileNameRel,featureLexiconFileNameRelList);
 i=find(isSameLexicon);  
-if isempty(i)
+if isempty(i) ,
   featureLexiconName='custom';
   %featureLexiconAnimalType='';
 else
@@ -27,27 +27,44 @@ else
   %featureLexiconAnimalType=featureLexiconAnimalTypeList{i};
 end
 
+% Convert featureparamlist, if present, to new format
+% If not present, the sublexicon is identical to the lexicon
+if isfield(projectParams,'featureparamlist') ,
+  sublexiconPFNames=fieldnames(projectParams.featureparamlist);
+else
+  sublexiconPFNames=fieldnames(featureLexicon.perframe);
+end
+
 % copy the project params over
 everythingParams.behaviors=projectParams.behaviors;
 fileParams=projectParams.file;
 % delete some field that we don't need/want
-if isfield(fileParams,'featureconfigfile')
+if isfield(fileParams,'featureconfigfile') ,
   fileParams=rmfield(fileParams,'featureconfigfile');
 end
 everythingParams.file=fileParams;
-everythingParams.trxGraphicParams=projectParams.trx;
-everythingParams.labelGraphicParams=projectParams.labels;
+if isfield(projectParams,'trx')
+  everythingParams.trxGraphicParams=projectParams.trx;
+else
+  everythingParams.trxGraphicParams=projectParams.plot.trx;
+end
+if isfield(projectParams,'labels')
+  everythingParams.labelGraphicParams=projectParams.labels;
+else
+  everythingParams.labelGraphicParams=projectParams.plot.labels;
+end
 everythingParams.featureLexiconName=featureLexiconName;
 everythingParams.featureLexicon=featureLexicon;
+everythingParams.sublexiconPFNames=sublexiconPFNames;
 everythingParams.scoresAsInput=projectParams.scoresinput;
-if isempty(projectParams.behaviors.names)                             
+if isempty(projectParams.behaviors.names) ,
   everythingParams.behaviorName='';
 else
   everythingParams.behaviorName=projectParams.behaviors.names{1};
 end
 
 % copy the experiment dirs, labels over
-if isempty(classifierParams)
+if isempty(classifierParams) ,
   % if no classifierParams provided
   everythingParams=appendEmptyLabelsAndDefaultClassifier(everythingParams,projectParams);
 else
@@ -58,4 +75,4 @@ end
 % set the version number
 everythingParams.ver='0.5.0';
 
-end                                    
+end
