746042902aa25dbdc0e1b6f491455a0d8a05d150
Adam L. Taylor <adamlyletaylor@gmail.com>
2013-04-15 18:59:18 -0400

    Fixed some issues with Show Similar Frames... UI, particularly on Mac.

--- misc/adjustButtonColorsIfMac.m
+++ misc/adjustButtonColorsIfMac.m
@@ -1,13 +1,13 @@
-function adjustNonLabelButtonColor(button)
+function adjustButtonColorsIfMac(buttons)
 % This adjust the color of non-label buttons for the platform.  On Windows
 % and Linux, this does nothing.  On Mac, it gives the buttons the default
 % colors.
 
 if ismac()
   defaultBackgroundColor = get(0,'defaultUicontrolBackgroundColor');
-  set(button,'ForegroundColor','k', ...
-             'BackgroundColor',defaultBackgroundColor, ...
-             'Cdata',[]);  
+  set(buttons,'ForegroundColor','k', ...
+              'BackgroundColor',defaultBackgroundColor, ...
+              'Cdata',[]);  
 end
                 
 end
