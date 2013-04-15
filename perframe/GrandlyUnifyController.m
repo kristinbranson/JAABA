e6bb39930229a506195ba19632001eba5c16d5ed
Adam L. Taylor <adamlyletaylor@gmail.com>
2013-04-15 14:26:57 -0400

    GUI for converting old-style files to .jab files now works if the files specified are all valid.

---
+++ perframe/GrandlyUnifyController.m
@@ -0,0 +1,75 @@
+classdef GrandlyUnifyController < handle
+  properties
+    model  % a ref to the model
+    view  % a ref to the view
+  end
+  methods
+    function self=GrandlyUnifyController()
+      self.model=GrandlyUnifyModel();
+      self.view=GrandlyUnifyView(self.model,self);
+    end  % constructor method
+
+    % ---------------------------------------------------------------------
+    function chooseProjectFileButtonPressed(self)
+      [fileNameRel,pathName] = ...
+        uigetfile({'*.mat','Matlab MAT files (*.mat)'}, ...
+                  'Choose JAABA Project File');
+      if fileNameRel == 0; return; end;
+      fileNameAbs = fullfile(pathName,fileNameRel);
+      self.model.projectFileName=fileNameAbs;
+      self.view.update();
+    end
+    
+    % ---------------------------------------------------------------------
+    function chooseClassifierFileButtonPressed(self)
+      [fileNameRel,pathName] = ...
+        uigetfile({'*.mat','Matlab MAT files (*.mat)'}, ...
+                  'Choose JAABA Classifier File');
+      if fileNameRel == 0; return; end;
+      fileNameAbs = fullfile(pathName,fileNameRel);
+      self.model.classifierFileName=fileNameAbs;
+      self.view.update();
+    end
+    
+    % ---------------------------------------------------------------------
+    function gtExpDirsAddButtonPressed(self)
+      dirNameAbs = ...
+        uigetdir('', ...
+                 'Add Ground-Truth Experiment Directory');
+      if dirNameAbs == 0; return; end;
+      self.model.addGTExpDirName(dirNameAbs);
+      self.view.update();
+    end
+    
+    % ---------------------------------------------------------------------
+    function gtExpDirsRemoveButtonPressed(self)
+      self.model.removeCurrentGTExpDirName();
+      self.view.update();      
+    end
+    
+    % ---------------------------------------------------------------------
+    function convertButtonPressed(self)
+      [fileNameRel, pathName] = ...
+        uiputfile({'*.jab','JAABA files (*.jab)'}, ...
+                  'Save JAABA File As...');
+      if fileNameRel == 0; return; end;  % user hit cancel
+      fileNameAbs = fullfile(pathName,fileNameRel);
+      projectFileName=self.model.projectFileName;
+      classifierFileName=self.model.classifierFileName;
+      gtExpDirNames=self.model.gtExpDirNames;
+      try
+        everythingFileFromOldStyleProjectAndClassifierFiles(...
+          fileNameAbs, ...
+          projectFileName, ...
+          classifierFileName, ...
+          gtExpDirNames);
+      catch excp
+        if isequal(excp.identifier,'MATLAB:load:notBinaryFile')
+          uiwait(errordlg(excp.message,'Error','modal'));
+        else
+          rethrow(excp);
+        end
+      end  % try/catch
+    end  % method
+  end
+end
---
+++ perframe/unify.m
@@ -0,0 +1,9 @@
+function varargout=unify()
+
+controller=GrandlyUnifyController();
+
+if nargout>=1
+  varargout{1}=controller;
+end
+
+end
