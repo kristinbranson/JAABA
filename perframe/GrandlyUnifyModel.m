fc5684d2922c549aa8138892d35beb54d2f79ab3
Adam L. Taylor <adamlyletaylor@gmail.com>
2013-05-23 17:53:03 -0400

    Changed unify() so that user can specify .jab files that correspond to classifier .mat files.

--- perframe/GrandlyUnifyModel.m
+++ perframe/GrandlyUnifyModel.m
@@ -2,6 +2,12 @@ classdef GrandlyUnifyModel < handle
   properties (GetAccess=public, SetAccess=private)
     projectFileName=''
       % The project file name to be converted.
+    scoreFeatureMatFileNames=cell(0,1);
+      % The file names of any score feature classifiers in the project file
+    scoreFeatureJabFileNames=cell(0,1);
+      % The names of the jab files that go with each score feature
+      % If the jab file name for a particular score feature is unspecified,
+      % that element will be the empty string.
     classifierFileName=''  
       % The classifier file name to be converted.
     gtExpDirNames={}
@@ -9,34 +15,81 @@ classdef GrandlyUnifyModel < handle
     iCurrentGTExpDir=[]
       % The index of the currently selected GT experiment dir.  Empty
       % iff gtExpDirNames is empty.
-%     workingDirName='';
-%       % the absolute path of the default dir used for file choosers, etc.
   end
+  
   methods
+    % ---------------------------------------------------------------------
     function self=GrandlyUnifyModel()
-      %self.workingDirName=pwd();
     end  % constructor method
+
+    % ---------------------------------------------------------------------
     function setProjectFileName(self,projectFileName)
-      self.projectFileName=projectFileName;
-      %self.workingDirName=fileparts(projectFileNameAbs);
+      oldProjectFileName=self.projectFileName;
+      oldScoreFeatureMatFileNames=self.scoreFeatureMatFileNames;
+      oldScoreFeatureJabFileNames=self.scoreFeatureJabFileNames;
+      try
+        self.projectFileName=projectFileName;
+        % Need to read project file, extract score feature names, if any.
+        [self.scoreFeatureMatFileNames,self.scoreFeatureJabFileNames]= ...
+          GrandlyUnifyModel.extractScoreFeatureNames(projectFileName);
+      catch excp
+        self.projectFileName=oldProjectFileName;
+        self.scoreFeatureMatFileNames=oldScoreFeatureMatFileNames;
+        self.scoreFeatureJabFileNames=oldScoreFeatureJabFileNames;
+        rethrow(excp);
+      end
     end
+
+    % ---------------------------------------------------------------------
+    function setScoreFeatureJabFileName(self,matFileName,jabFileName)
+      i=whichstr(matFileName,self.scoreFeatureMatFileNames);
+      if isempty(i)
+        error('GrandlyUnifyModel:noSuchScoreFeatureMatFileName', ...
+              sprintf('No score feature mat file name %s.',matFileName));  %#ok
+      else
+        self.scoreFeatureJabFileNames{i}=jabFileName;
+      end
+    end
+
+    % ---------------------------------------------------------------------
     function setClassifierFileName(self,fileName)
-      %fileNameAbs=absolutifyFileName(fileName,self.workingDirName);
       self.classifierFileName=fileName;
-      %self.workingDirName=fileparts(fileNameAbs);
     end
+
+    % ---------------------------------------------------------------------
     function result=isProjectFileSpecified(self)
       result=~isempty(self.projectFileName);
     end
+
+    % ---------------------------------------------------------------------
+    function result=isScoreFeatureJabFileSpecified(self,index)
+      if ischar(index)
+        matFileName=index;
+        i=whichstr(matFileName,self.scoreFeatureMatFileNames);
+        if isempty(i)
+          error('GrandlyUnifyModel:noSuchScoreFeatureMatFileName', ...
+                sprintf('No score feature mat file name %s.',matFileName));  %#ok
+        end
+      else
+        i=index;
+      end
+      result=~isempty(self.scoreFeatureJabFileNames{i});
+    end
+
+    % ---------------------------------------------------------------------
     function result=isClassifierFileSpecified(self)
       result=~isempty(self.classifierFileName);
     end
+
+    % ---------------------------------------------------------------------
     function addGTExpDirName(self,dirName)
       %dirNameAbs=absolutifyFileName(dirName,self.workingDirName);
       self.gtExpDirNames{end+1}=dirName;
       self.iCurrentGTExpDir = length(self.gtExpDirNames);
       %self.workingDirName=fileparts(dirName);
     end
+
+    % ---------------------------------------------------------------------
     function removeCurrentGTExpDirName(self)
       i = self.iCurrentGTExpDir;
       if isempty(i), return; end
@@ -49,6 +102,8 @@ classdef GrandlyUnifyModel < handle
       end
       self.iCurrentGTExpDir=i;
     end  % method
+
+    % ---------------------------------------------------------------------
     function convert(self,jabFileName)
       %jabFileNameAbs=absolutifyFileName(jabFileName,self.workingDirName);
       everythingFileFromOldStyleProjectAndClassifierFiles(...
@@ -58,4 +113,18 @@ classdef GrandlyUnifyModel < handle
         self.gtExpDirNames);    
     end  % method
   end  % methods
+
+  methods (Static)
+    % ---------------------------------------------------------------------
+    function [scoreFeatureMatFileNames,scoreFeatureJabFileNames]=extractScoreFeatureNames(projectFileName)
+      projectParams=load(projectFileName,'-mat');
+      scoresAsInput=projectParams.scoresinput;
+      scoreFeatureMatFileNames={scoresAsInput.classifierfile}';
+      nScoreFeatures=length(scoresAsInput);
+      scoreFeatureJabFileNames=cell(nScoreFeatures,1);
+      if nScoreFeatures>0
+        scoreFeatureJabFileNames{:}='';
+      end
+    end % method
+  end  % class methods
 end  % classdef
