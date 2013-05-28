9127d1251fcc09ae04c5ea81e06541c77bf1263e
Kristin Branson <bransonk@janelia.hhmi.org>
2013-05-28 14:54:49 -0400

    After creating a new project, user is now prompted to add experiments if there are none. Worked on documentation.

--- perframe/ProjectSetup.m
+++ perframe/ProjectSetup.m
@@ -124,14 +124,14 @@ return
 
 % -------------------------------------------------------------------------
 % --- Outputs from this function are returned to the command line.
-function ProjectSetup_OutputFcn(hObject, ~, handles)  %#ok
+function varargout = ProjectSetup_OutputFcn(hObject, ~, handles)  %#ok
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
 % Get default command line output from handles structure
-%varargout{1} = handles.output;
+varargout{1} = handles.output;
 %varargout{2} = handles.outfile;
 %delete(handles.figureProjectSetup);
 return
