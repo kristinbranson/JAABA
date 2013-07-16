e70a86ad71e1788ff4b9a38b346b9396a0a605aa
Kristin Branson <bransonk@janelia.hhmi.org>
2013-07-16 10:01:32 -0400

    Modified MakeJAABAResultsMovie_PositiveClips.m to use postprocessed instead of t0s and t1s. Updated read_ann_line to read more parameters. Added option of setting output directory for make_jlabel_results_movie.

--- filehandling/read_ann_line.m
+++ filehandling/read_ann_line.m
@@ -11,26 +11,52 @@ if strcmp(s,'end header'),
   return;
 end
 
+% if isspecial,
+%   sz = str2num(value);
+%   value = fread(fid,sz,'uint8');
+% else
+%   tmp = str2num(value);
+%   if ~isempty(tmp),
+%     value = tmp;
+%   end;
+% end;
+
 i = findstr(s,':');
 if isempty(i),
   return;
 end;
-param = s(1:i-1);
-value = s(i+1:end);
+param = s(1:i(1)-1);
+value = s(i(1)+1:end);
 
 specialparams = {'background median','background mean',...
                  'background mad','background std',...
-                 'hfnorm','roipolygons'};
+                 'hfnorm','fracframesisback',...
+                 'background center','background dev',...
+                 'isarena'};
+stringparams = {'bg_algorithm','version','expbgfgmodel_filename','movie_name','data format'};
+pickledparams = {'roipolygons'};
+maybestringparams = {'bg_type'};
 isspecial = ismember(param,specialparams);
+isstring = ismember(param,stringparams);
+ispickled = ismember(param,pickledparams);
+maybestring = ismember(param,maybestringparams);
 
 if isspecial,
-  sz = str2num(value);
+  sz = str2double(value);
   value = fread(fid,sz,'uint8');
+elseif ispickled,
+  sz = str2double(value);
+  value = fread(fid,sz,'char');  
+elseif isstring,
+  % leave value as string
+elseif maybestring,
+  nvalue = str2double(value);
+  if ~isnan(nvalue),
+    value = nvalue;
+  end
 else
-  tmp = str2num(value);
+  tmp = str2double(value);
   if ~isempty(tmp),
     value = tmp;
   end;
 end;
-
-param = strrep(param,' ','_');
