a8c763f45b5de278b9989028c713e1fe428eefb0
Allen Lee <allenleetc@gmail.com>
2014-12-24 13:41:20 -0500

    Milestone1, cleanups, regression testing

--- perframe/LabelsPlot.m
+++ perframe/LabelsPlot.m
@@ -2,14 +2,17 @@ classdef LabelsPlot
   
   methods (Static)
     
-    function labels_plot = labelsPlot(t0,t1,nTL,nBeh,nflies)      
+    function labels_plot = labelsPlot(t0,t1,nTL,nBeh,beh2TL,TL2beh,nflies)      
       n = t1-t0+1;      
 
       labels_plot = struct();      
       labels_plot.n = n;
       labels_plot.off = 1-t0;
-      labels_plot.nbeh = nBeh;
       labels_plot.nflies = nflies;
+      labels_plot.nbeh = nBeh;
+      labels_plot.nTL = nTL;
+      labels_plot.beh2TL = beh2TL;
+      labels_plot.TL2beh = TL2beh;
 
       labels_plot.im = zeros(nTL,n,3);
       labels_plot.predicted_im = zeros(nTL,n,3); % TODO: multiclass shortcut: just show raw scores, plot gets busy
@@ -24,13 +27,31 @@ classdef LabelsPlot
       labels_plot.predy = nan(2,n,nBeh,nflies);      
     end
     
-    function labels_plot = labelsPlotInitXY(labels_plot,labelidx,ifly,x,y)
+    function labels_plot = labelsPlotInitXY(labels_plot,ifly,x,y,...
+        labelidx,predictedidx,scores,confThreshs)
+      % ifly: scalar fly index (into eg labels_plot.x(:,:,:,ifly)
+      % x, y: vectors, track positions
+      % labelidx: .vals field from labelIdx structure. Values are in
+      %   1:labels_plot.nbeh      % 
+      % predictedidx: optional, can be [] if no prediction. Values are in
+      %   {0,1,2,1.5}, see JLabelData.GetPredictedIdx.
+      % scores: optional, can be [] if no prediction. See GetPredictedIdx.
+      % confThreshs: vector of length labels_plot.nbeh, or [] if no pred
+      
+      %MERGESTUPDATED
+      
+      assert(isscalar(ifly) && any(ifly==1:labels_plot.nflies));
       assert(isequal(numel(x),numel(y),labels_plot.n));
-      assert(ismember(ifly,1:labels_plot.nflies));
+      assert(isequal([labels_plot.nTL labels_plot.n],size(labelidx)));
+      tfPred = ~isempty(predictedidx);
+      if tfPred
+        assert(isequal([labels_plot.nTL labels_plot.n],size(predictedidx),size(scores)));
+        assert(isvector(confThreshs)&&numel(confThreshs)==labels_plot.nbeh);
+      end
       
       for ibeh = 1:labels_plot.nbeh
-        tfBeh = labelidx.vals==ibeh; 
-        tfBeh = any(tfBeh,1); % Doesn't matter which timeline(s) the behavior is labeled in
+        iTL = labels_plot.beh2TL(ibeh);
+        tfBeh = labelidx(iTL,:)==ibeh; 
         
         idx = find(tfBeh);
         idx1 = min(idx+1,labels_plot.n);
@@ -38,19 +59,31 @@ classdef LabelsPlot
         labels_plot.x(2,idx,ibeh,ifly) = x(idx1);
         labels_plot.y(1,idx,ibeh,ifly) = y(idx);
         labels_plot.y(2,idx,ibeh,ifly) = y(idx1);
-        
-        % ALXXX MINIMAL this can be true see JLabel.SetCurrentFlies
-        classifierPresent = false;
-        if classifierPresent,
-          assert(false,'ALXXX');
-          % idx = find(predictedidx == behaviori);
-          idx = find((predictedidx == ibeh) & ...
-            (abs(scores)>data.GetConfidenceThreshold(ibeh)));
-          idx1 = min(idx+1,numel(x));
-          labels_plot.predx(1,idx,ibeh,ifly) = x(idx);
-          labels_plot.predx(2,idx,ibeh,ifly) = x(idx1);
-          labels_plot.predy(1,idx,ibeh,ifly) = y(idx);
-          labels_plot.predy(2,idx,ibeh,ifly) = y(idx1);
+      end
+      if tfPred
+        for iTL = 1:labels_plot.nTL
+          iBehs = labels_plot.TL2beh{iTL};
+          assert(numel(iBehs)==2);
+          iBehPos = iBehs(1);
+          iBehNeg = iBehs(2);
+          
+          idxPos = predictedidx(iTL,:)==1 & ...
+                    abs(scores(iTL,:))>confThreshs(iBehPos);
+          idxNeg = predictedidx(iTL,:)==2 & ...
+                    abs(scores(iTL,:))>confThreshs(iBehNeg);            
+          idxPos = find(idxPos);
+          idxNeg = find(idxNeg);
+          idxPos1 = min(idxPos+1,labels_plot.n);
+          idxNeg1 = min(idxNeg+1,labels_plot.n);
+          
+          labels_plot.predx(1,idxPos,iBehPos,ifly) = x(idxPos);
+          labels_plot.predx(2,idxPos,iBehPos,ifly) = x(idxPos1);
+          labels_plot.predx(1,idxNeg,iBehNeg,ifly) = x(idxNeg);
+          labels_plot.predx(2,idxNeg,iBehNeg,ifly) = x(idxNeg1);
+          labels_plot.predy(1,idxPos,iBehPos,ifly) = x(idxPos);
+          labels_plot.predy(2,idxPos,iBehPos,ifly) = x(idxPos1);
+          labels_plot.predy(1,idxNeg,iBehNeg,ifly) = x(idxNeg);
+          labels_plot.predy(2,idxNeg,iBehNeg,ifly) = x(idxNeg1);          
         end
       end
     end
