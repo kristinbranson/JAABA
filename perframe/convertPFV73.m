4b0d0ed6824df4a250b1c481ddc5a0d566a3232b
Mayank Home Workstation <mayank@ws.home>
2015-07-31 19:02:08 +0530

    Made changes to make reading of perframe files faster

---
+++ perframe/convertPFV73.m
@@ -0,0 +1,17 @@
+function convertPFV73(expdir)
+
+dd = dir(fullfile(expdir,'perframe','*.mat'));
+
+fprintf('Converting %d perframe files\n',numel(dd));  
+
+for ndx = 1:numel(dd)
+  pfname = fullfile(expdir,'perframe',dd(ndx).name);
+  ss = fopen(pfname,'r');
+  qq = fread(ss,10,'*char')';
+  fclose(ss);
+  
+  if ~strcmp(qq(8:10),'7.3')
+    Q = load(pfname);
+    save(pfname,'-struct','Q','-v7.3');
+  end
+end
