1f8d561a35ee3c457e4497ee4fcfd4b395763ad3
Kristin Branson <bransonk@janelia.hhmi.org>
2017-02-15 12:47:17 -0500

    Modified axisalmosttight to accept more than one set of axes. Modified win2unixpath to take an extra optional argument.

--- misc/axisalmosttight.m
+++ misc/axisalmosttight.m
@@ -5,9 +5,18 @@ function axisalmosttight(border,varargin)
 if nargin < 1 || isempty(border),
   border = 1/20;
 end
+if numel(varargin)>1,
+  hax = varargin{1};
+else
+  hax = gca;
+end
 
-axis(varargin{:},'tight');
-ax = axis(varargin{:});
+ax = [inf,-inf,inf,-inf];
+for i = 1:numel(hax),
+  axis(hax(i),'tight');
+  ax1 = axis(hax);
+  ax = [min(ax(1),ax1(1)),max(ax(2),ax1(2)),min(ax(3),ax1(3)),max(ax(4),ax1(4))];
+end
 dx = (ax(2) - ax(1))*border;
 ax(1) = ax(1) - dx;
 ax(2) = ax(2) + dx;
@@ -15,4 +24,6 @@ dy = (ax(4) - ax(3))*border;
 ax(3) = ax(3) - dy;
 ax(4) = ax(4) + dy;
 
-axis(varargin{:},ax);
+for i = 1:numel(hax),
+  axis(hax(i),ax);
+end
