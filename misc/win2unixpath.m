ed6cf9be6a079ea537850be8eefb877f130ed6f4
Kristin Branson <bransonk@janelia.hhmi.org>
2017-02-15 12:47:17 -0500

    Modified axisalmosttight to accept more than one set of axes. Modified win2unixpath to take an extra optional argument.

--- misc/win2unixpath.m
+++ misc/win2unixpath.m
@@ -1,8 +1,16 @@
-function filepaths = win2unixpath(filepaths,rootdir)
+function filepaths = win2unixpath(filepaths,unixrootdir,winrootdir)
 
-if rootdir(end) ~= '/'
-  rootdir(end+1) = '/';
+if unixrootdir(end) ~= '/'
+  unixrootdir(end+1) = '/';
 end
 
-filepaths = regexprep(filepaths,'^[A-Z]:\\',rootdir);
+if nargin < 3,
+  filepaths = regexprep(filepaths,'^[A-Z]:\\',unixrootdir);
+else
+
+  if winrootdir(end) ~= '\',
+    winrootdir(end+1) = '\';
+  end
+  filepaths = regexprep(filepaths,['^',strrep(strrep(winrootdir,'\','\\'),'$','\$')],unixrootdir);
+end
 filepaths = strrep(filepaths,'\','/');
