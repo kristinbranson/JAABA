a1a181a65b830198658d7d923a2175b4f2aa6d8a
Kristin Branson <bransonk@janelia.hhmi.org>
2017-11-08 14:28:37 -0500

    Added h5 reading to get_readframe_fcn.

--- misc/getframe_invisible.m
+++ misc/getframe_invisible.m
@@ -100,7 +100,7 @@ warning(warnstate);
 
 
 function inputType = getInputType(frame)
-if isscalar(frame) && ishandle(frame) && (frame > 0)
+if isscalar(frame) && ishandle(frame),
   inputType = get(frame,'type');
 elseif isstruct(frame) & isfield(frame,'cdata')
   inputType = 'movie';
