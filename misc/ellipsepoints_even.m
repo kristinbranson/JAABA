77509c1bbfc05f515acc18b18fee7e31abbd87c2
Alice <arobie@gmail.com>
2020-10-28 18:00:09 -0400

    code to compute angleonclosestfly2 for Female Aggression paper

---
+++ misc/ellipsepoints_even.m
@@ -0,0 +1,63 @@
+% [X,Y] = ellipsepoints(a,b,x0,y0,phi)
+function [X,Y,theta] = ellipsepoints_even(a,b,x0,y0,phi,npoints)
+
+if (nargin < 2)||(nargin > 6),
+    error('Usage: [X,Y,theta] = ellipsepoints(a,b,x0,y0,phi,[npoints])');
+    
+elseif nargin == 2
+    x0 = 0;     y0 = 0;
+    phi = 0;    
+    
+elseif nargin == 3
+  phi = x0;
+  x0 = 0; y0 = 0;
+    
+elseif nargin == 4     
+    phi = 0;    
+    
+end
+
+if nargin < 6,
+  npoints = 60;
+end
+
+% persistent ELLIPSEPOINTS_CACHEDDATA_THETA
+% if ~isempty(ELLIPSEPOINTS_CACHEDDATA_THETA) && numel(ELLIPSEPOINTS_CACHEDDATA_THETA) == npoints,
+%   theta = ELLIPSEPOINTS_CACHEDDATA_THETA;
+% else
+%   theta = linspace(0,2*pi,npoints);
+%   ELLIPSEPOINTS_CACHEDDATA_THETA = theta;
+% end
+
+theta = linspace(0,2*pi,npoints);
+
+
+% Parametric equation of the ellipse
+%----------------------------------------
+ x = a*cos(theta);
+ y = b*sin(theta);
+
+ % Find evenly spaced points
+ p = [x;y]';     
+ q = curvspace(p,npoints); 
+ x = q(:,1);
+ y = q(:,2);
+ 
+ nm = a.*y;
+ dm = b.*x;
+ thetanew = atan2(nm,dm);
+ 
+ % debuging
+%  figure, plot(x,y,'.'), axis equal, hold on
+%  tempx = a* cos(thetanew);
+%  tempy = b* sin(thetanew); 
+%  plot([tempx],[tempy],'mo')
+% change range to 0 : pi, 0 = nose, pi = tail 
+ theta = abs(thetanew); 
+ 
+% Coordinate transform 
+%----------------------------------------
+ X = cos(phi)*x - sin(phi)*y;
+ Y = sin(phi)*x + cos(phi)*y;
+ X = X + x0;
+ Y = Y + y0;
