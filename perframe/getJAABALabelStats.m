79a9e819b222fad1842ba11a4c5769baf5424ad4
Kristin Branson <bransonk@janelia.hhmi.org>
2021-11-08 09:51:12 -0500

    Modified getJAABALabelStats to take in info about label type for each experiment. Also print out per-experiment info.

--- perframe/getJAABALabelStats.m
+++ perframe/getJAABALabelStats.m
@@ -1,4 +1,4 @@
-function info = getJAABALabelStats()
+function info = getJAABALabelStats(expinfo)
 %%
 hfig = findall(0,'type','figure','Name','JAABA');
 assert(numel(hfig)==1);
@@ -13,19 +13,31 @@ nonebehavior = data.nobehaviornames;
 info.allnames = [behaviors,nonebehavior];
 nbehaviors = numel(info.allnames);
 
-expinfo = regexp(expnames,'^(?<type>.*)_Rig(?<rig>\w)_(?<timestamp>\d{8}T\d{6})$','names','once');  
+if nargin >= 1,
+  expnames0 = cellfun(@fileBaseName,{expinfo.file_system_path},'Uni',0);
+  [ism,idx] = ismember(expnames,expnames0);
+  assert(all(ism));
+  expinfo = expinfo(idx);  
+else
+  expinfo = regexp(expnames,'^(?<label>.*)_Rig(?<rig>\w)_(?<timestamp>\d{8}T\d{6})$','names','once');
+  expinfo = [expinfo{:}];
+end
   
-expinfo = [expinfo{:}];
-[info.types,~,typeidx] = unique({expinfo.type});
+[info.labels,~,labelidx0] = unique({expinfo.label});
 
-info.nlabelframes = zeros(numel(info.types),nbehaviors);
-info.nlabelbouts = zeros(numel(info.types),nbehaviors);
-info.nlabelflies = zeros(numel(info.types),nbehaviors);
-info.nlabelexps = zeros(numel(info.types),nbehaviors);
+info.nlabelframes = zeros(numel(info.labels),nbehaviors);
+info.nlabelbouts = zeros(numel(info.labels),nbehaviors);
+info.nlabelflies = zeros(numel(info.labels),nbehaviors);
+info.nlabelexps = zeros(numel(info.labels),nbehaviors);
+info.ntotalexps = zeros(numel(info.labels),1);
+info.nlabelframesperexp = zeros(numel(expinfo),nbehaviors);
+info.nlabelboutsperexp = zeros(numel(expinfo),nbehaviors);
+info.nlabelfliesperexp = zeros(numel(expinfo),nbehaviors);
 
-for typei = 1:numel(info.types),
+for labeli = 1:numel(info.labels),
   
-  idxcurr = find(typeidx==typei);
+  idxcurr = find(labelidx0==labeli);
+  info.ntotalexps(labeli) = numel(idxcurr);
   for jj = 1:numel(idxcurr),
     islabeledexp = false(1,nbehaviors);
     expi = idxcurr(jj);
@@ -37,28 +49,43 @@ for typei = 1:numel(info.types),
       [ism,labelidx] = ismember(labels(expi).names{flyi},info.allnames);
       assert(all(ism));
       counts = hist(labelidx,1:nbehaviors);      
-      info.nlabelflies(typei,counts>0) = info.nlabelflies(typei,counts>0) + 1;
-      info.nlabelbouts(typei,:) = info.nlabelbouts(typei,:) + counts;
+      info.nlabelflies(labeli,counts>0) = info.nlabelflies(labeli,counts>0) + 1;
+      info.nlabelbouts(labeli,:) = info.nlabelbouts(labeli,:) + counts;
+      info.nlabelfliesperexp(expi,counts>0) = info.nlabelfliesperexp(expi,counts>0) + 1;
+      info.nlabelboutsperexp(expi,:) = info.nlabelboutsperexp(expi,:) + counts;
       islabeledexp(counts>0) = true;
       for behi = 1:nbehaviors,
-        info.nlabelframes(typei,behi) = info.nlabelframes(typei,behi) + sum((labels(expi).t1s{flyi}(labelidx==behi)-labels(expi).t0s{flyi}(labelidx==behi)));
+        ncurr = sum((labels(expi).t1s{flyi}(labelidx==behi)-labels(expi).t0s{flyi}(labelidx==behi)));
+        info.nlabelframes(labeli,behi) = info.nlabelframes(labeli,behi) + ncurr;
+        info.nlabelframesperexp(expi,behi) = info.nlabelframesperexp(expi,behi) + ncurr;
       end
     end
-    info.nlabelexps(typei,islabeledexp) = info.nlabelexps(typei,islabeledexp) + 1;
+    info.nlabelexps(labeli,islabeledexp) = info.nlabelexps(labeli,islabeledexp) + 1;
   end
   
 end
 
-typelens = cellfun(@numel,info.types);
-maxtypelen = max(typelens);
+fprintf('Labels per experiment type:\n');
+labellens = cellfun(@numel,info.labels);
+maxlabellen = max(labellens);
 namelens = cellfun(@numel,info.allnames);
 maxnamelen = max(namelens);
-for typei = 1:numel(info.types),
+for labeli = 1:numel(info.labels),
   for behi = 1:nbehaviors,
-    fprintf('%s%s %s%s:\t',info.types{typei},repmat(' ',[1,maxtypelen-typelens(typei)]),...
+    fprintf('%s%s %s%s:\t',info.labels{labeli},repmat(' ',[1,maxlabellen-labellens(labeli)]),...
       info.allnames{behi},repmat(' ',[1,maxnamelen-namelens(behi)]));
-    fprintf('%2d exps, %3d flies, %6d bouts, %6d frames\n',info.nlabelexps(typei,behi),...
-      info.nlabelflies(typei,behi),info.nlabelbouts(typei,behi),info.nlabelframes(typei,behi));
+    fprintf('%2d/%2d exps, %3d flies, %6d bouts, %6d frames\n',info.nlabelexps(labeli,behi),info.ntotalexps(labeli),...
+      info.nlabelflies(labeli,behi),info.nlabelbouts(labeli,behi),info.nlabelframes(labeli,behi));
   end
 end
 
+fprintf('\nLabels per experiment:\n');
+expnamelens = cellfun(@numel,expnames);
+maxexpnamelen = max(expnamelens);
+for expi = 1:numel(expinfo),
+  for behi = 1:nbehaviors,
+    fprintf('%s%s %s%s:\t',expnames{expi},repmat(' ',[1,maxexpnamelen-expnamelens(expi)]),...
+      info.allnames{behi},repmat(' ',[1,maxnamelen-namelens(behi)]));
+    fprintf('%3d flies, %6d bouts, %6d frames\n',info.nlabelfliesperexp(expi,behi),info.nlabelboutsperexp(expi,behi),info.nlabelframesperexp(expi,behi));
+  end
+end
