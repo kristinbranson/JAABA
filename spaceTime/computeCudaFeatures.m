ab9a6a1816836811146f456b2ae92b6f2891e5e1
Rutuja <27rutuja@gmail.com>
2023-05-22 18:17:45 -0400

    Added argument to computeCudaFeatures.m

--- spaceTime/computeCudaFeatures.m
+++ spaceTime/computeCudaFeatures.m
@@ -1,9 +1,10 @@
 function computeCudaFeatures(pathtocudascript, exp_list, varargin)
 
-  [bash_path, compute_features, combine_movie] = myparse(varargin,...
+  [bash_path, compute_features, combine_movie, convertfeattoMatlab] = myparse(varargin,...
                 'bash_path', '"C:\Program Files\Git\bin\bash.exe"',...
                 'compute_features', '1',...
-                'combine_movie', '1');
+                'combine_movie', '1',...
+                'convertfeattoMatlab', '1');
 
   fullpathtoscript = pathtocudascript;
   runcommandtoscript = [bash_path ' ' fullpathtoscript ' ' exp_list ' ' compute_features ' ' combine_movie];
@@ -16,6 +17,9 @@ function computeCudaFeatures(pathtocudascript, exp_list, varargin)
   %cmdStr = ['"C:\Program Files\Git\bin\bash.exe" C:\Users\27rut\BIAS\scripts\run_bias.sh Y:\hantman_data\jab_experiments\STA14\STA14\20230503\hantman_modified.txt'];
 
   % convert cuda features to features.mat
-  convertFeaturestoMatlab(exp_list)
-  
+  convertfeattoMatlab = str2double(convertfeattoMatlab);
+  if(convertfeattoMatlab)
+      disp(convertfeattoMatlab)
+      convertFeaturestoMatlab(exp_list)
+  end
 end
